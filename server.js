a6941a51dea3b61602c4b865f0464ddafbfc6b5c
suraj5789 <suraj.gupta23@gmail.com>
2021-04-17 12:56:11 +0100

    added api

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/getPing', (req, res) => {
+  res.send(`Hello World! ${process.env.env}`)
 })
 
 app.listen(port, () => {
